7744c6c9aa8ec16221cb75063527b5a69904c661
Fython <fang.jia.hui123@gmail.com>
2017-10-20 17:23:27 +0800

    reddit crawl

---
+++ tophub/spiders/reddit_spider.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+import scrapy
+from ..items import RedditItem
+
+
+class RedditSpider(scrapy.Spider):
+    name = 'reddit'
+    start_urls = ['https://www.reddit.com/r/programming/']
+
+    def extract_strip(self, target, expr):
+        return target.css(expr).extract_first().strip()
+
+    def parse(self, response):
+        for post in response.css(".thing"):
+            item = RedditItem()
+            item['title'] = self.extract_strip(post, "a.title::text")
+            item['link'] = self.extract_strip(post, "a.title::attr(href)")
+            item['scores'] = self.extract_strip(post, ".score.unvoted::text")
+            item['comments'] = post.css("li.first a::text").re_first(r'(\d*) ')
+
+            yield item
+
+        next_page = response.css(
+            'span.next-button a::attr(href)').extract_first()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
